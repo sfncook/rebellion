87e8a5a0e28407767e3ed25f4972633a41aa19a0
Shawn Cook <sfncook@gmail.com>
2020-11-14 20:30:22 -0800

    cleanup

--- core/src/main/java/com/rebllelionandroid/core/database/gamestate/GameStateRepository.kt
+++ core/src/main/java/com/rebllelionandroid/core/database/gamestate/GameStateRepository.kt
@@ -1,22 +1,12 @@
 package com.rebllelionandroid.core.database.gamestate
 
-import androidx.annotation.VisibleForTesting
-import androidx.annotation.VisibleForTesting.PRIVATE
 import androidx.lifecycle.LiveData
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
-import com.rebllelionandroid.core.database.gamestate.GameState
-import com.rebllelionandroid.core.database.gamestate.GameStateDao
 import javax.inject.Inject
 
 class GameStateRepository @Inject constructor(
-    @VisibleForTesting(otherwise = PRIVATE)
-    internal val gameStateDao: GameStateDao
+    private val gameStateDao: GameStateDao
 ) {
-
-
-    fun getAllCharactersFavoriteLiveData(): LiveData<List<GameState>> =
+    fun getGameState(): LiveData<List<GameState>> =
             gameStateDao.getAllGameStates()
 
     fun createNewGameState(gameState: GameState) =
