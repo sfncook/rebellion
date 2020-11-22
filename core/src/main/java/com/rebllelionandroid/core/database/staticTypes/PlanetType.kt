445c063159b592d5a2435cf8914c0ded3a7ed1ee
Shawn Cook <sfncook@gmail.com>
2020-11-22 13:04:35 -0800

    Fix planet type

--- core/src/main/java/com/rebllelionandroid/core/database/staticTypes/PlanetType.kt
+++ core/src/main/java/com/rebllelionandroid/core/database/staticTypes/PlanetType.kt
@@ -4,15 +4,15 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
 @Entity(
         tableName = "planet_types",
         foreignKeys = [ForeignKey(entity = SectorType::class,
                 parentColumns = arrayOf("id"),
-                childColumns = arrayOf("sector_id"),
-                onDelete = ForeignKey.CASCADE)])
+                childColumns = arrayOf("sector_id"))])
 data class PlanetType(
         @PrimaryKey val id: Long,
-        @ColumnInfo(name = "name") val name: String,
-        @ColumnInfo(name = "sector_id") val sectorId: Long
+        @NotNull @ColumnInfo(name = "name") val name: String,
+        @NotNull @ColumnInfo(name = "sector_id") val sectorId: Long
 )
