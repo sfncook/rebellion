f2fbcf22d3cd3d9c85f6de073a9635f7a60da4d5
Shawn Cook <sfncook@gmail.com>
2020-11-22 13:19:07 -0800

    Fix unit types

--- core/src/main/java/com/rebllelionandroid/core/database/staticTypes/UnitType.kt
+++ core/src/main/java/com/rebllelionandroid/core/database/staticTypes/UnitType.kt
@@ -4,12 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "unit_types")
 data class UnitType(
         @PrimaryKey val id: Long,
-        @ColumnInfo(name = "name") val name: String,
-        @ColumnInfo(name = "is_ship") val isShip: Int,
-        @ColumnInfo(name = "is_personelle") val isPersonelle: Int,
-        @ColumnInfo(name = "ship_capacity") val shipCapacity: Int
+        @NotNull @ColumnInfo(name = "name") val name: String,
+        @NotNull @ColumnInfo(name = "is_ship") val isShip: Int,
+        @NotNull @ColumnInfo(name = "is_personelle") val isPersonelle: Int,
+        @NotNull @ColumnInfo(name = "ship_capacity") val shipCapacity: Int
 )
