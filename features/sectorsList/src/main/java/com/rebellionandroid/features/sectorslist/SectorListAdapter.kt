943c175575c21a2599c607ec7ded5c11c0745237
Shawn Cook <sfncook@gmail.com>
2020-11-26 20:42:55 -0800

    sector click handler

--- features/sectorsList/src/main/java/com/rebellionandroid/features/sectorslist/SectorListAdapter.kt
+++ features/sectorsList/src/main/java/com/rebellionandroid/features/sectorslist/SectorListAdapter.kt
@@ -8,32 +8,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.rebllelionandroid.core.database.gamestate.SectorWithPlanets
 import com.rebllelionandroid.features.sectorsList.R
 
+
 class SectorListAdapter(
-        private val sectors: List<SectorWithPlanets>
+    private val sectors: List<SectorWithPlanets>
 ) : RecyclerView.Adapter<SectorListAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, private val sectorsWithPlanets: List<SectorWithPlanets>) : RecyclerView.ViewHolder(view) {
         val sectorName: TextView = view.findViewById(R.id.sector_name)
         val planetsList: RecyclerView  = view.findViewById(R.id.planets_list)
 
         init {
-            // Define click listener for the ViewHolder's View.
+            view.setOnClickListener {
+                val sectorWithPlanets = sectorsWithPlanets[adapterPosition]
+                println("click sector ${sectorWithPlanets.sector.name}")
+            }
         }
     }
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.list_item_sector, viewGroup, false)
-        return ViewHolder(view)
+        return ViewHolder(view, sectors)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val sector = sectors[position].sector
         viewHolder.sectorName.text = sector.name
