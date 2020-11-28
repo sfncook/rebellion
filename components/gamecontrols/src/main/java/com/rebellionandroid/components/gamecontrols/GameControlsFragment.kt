0795932e6155982c1aed55ba785b7a80f56e68b5
Shawn Cook <sfncook@gmail.com>
2020-11-28 11:03:19 -0800

    day data binding

--- components/gamecontrols/src/main/java/com/rebellionandroid/components/gamecontrols/GameControlsFragment.kt
+++ components/gamecontrols/src/main/java/com/rebellionandroid/components/gamecontrols/GameControlsFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.rebellionandroid.components.gamecontrols.databinding.FragmentGameControlsBinding
 import com.rebllelionandroid.core.BaseActivity
 import com.rebllelionandroid.core.GameStateViewModel
 import com.rebllelionandroid.core.di.DaggerGameStateComponent
@@ -26,11 +28,10 @@ class GameControlsFragment: Fragment() {
         val gameStateComponent = (activity as BaseActivity).gameStateComponent
         DaggerGameControlsComponent.factory().create(gameStateComponent).inject(this)
 
-        return inflater.inflate(R.layout.fragment_game_controls, container, false)
+//        return inflater.inflate(R.layout.fragment_game_controls, container, false)
+        val viewBinding: FragmentGameControlsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_controls, container, false)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
+        return viewBinding.root
     }
-
-//    override fun onAttach(context: Context) {
-//        super.onAttach(context)
-//    }
 
 }
