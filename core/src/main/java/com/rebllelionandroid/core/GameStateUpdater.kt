73044249b8ba718390c0a7a972e7b70f97394138
Shawn Cook <sfncook@gmail.com>
2020-11-28 15:30:55 -0800

    Delete GameStateUpdater

--- core/src/main/java/com/rebllelionandroid/core/GameStateUpdater.kt
+++
@@ -1,9 +0,0 @@
-package com.rebllelionandroid.core
-
-import com.rebllelionandroid.core.database.gamestate.GameStateRepository
-import javax.inject.Inject
-
-class GameStateUpdater @Inject constructor(
-        private val gameStateRepository: GameStateRepository
-) {
-}
--- core/src/main/java/com/rebllelionandroid/core/di/GameStateComponent.kt
+++ core/src/main/java/com/rebllelionandroid/core/di/GameStateComponent.kt
@@ -1,7 +1,6 @@
 package com.rebllelionandroid.core.di
 
 import android.content.Context
-import com.rebllelionandroid.core.GameStateUpdater
 import com.rebllelionandroid.core.GameStateViewModel
 import com.rebllelionandroid.core.di.modules.ContextModule
 import com.rebllelionandroid.core.di.modules.DatabaseModule
@@ -14,7 +13,6 @@ import dagger.Component
     ]
 )
 interface GameStateComponent {
-    fun gameStateUpdater(): GameStateUpdater
     fun gameStateViewModel(): GameStateViewModel
     fun context(): Context
 }
