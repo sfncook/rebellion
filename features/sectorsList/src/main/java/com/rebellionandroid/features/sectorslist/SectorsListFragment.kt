ff223279ae50d8d6964db514f9a87354083acd5b
Shawn Cook <sfncook@gmail.com>
2020-12-04 21:38:57 -0800

    load game state in sector detail

--- features/sectorsList/src/main/java/com/rebellionandroid/features/sectorslist/SectorsListFragment.kt
+++ features/sectorsList/src/main/java/com/rebellionandroid/features/sectorslist/SectorsListFragment.kt
@@ -1,20 +1,24 @@
 package com.rebellionandroid.features.sectorslist
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
-import androidx.navigation.findNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
+import androidx.lifecycle.lifecycleScope
+import com.rebllelionandroid.core.BaseActivity
 import com.rebllelionandroid.core.database.gamestate.GameStateWithSectors
 import com.rebllelionandroid.features.sectorsList.R
-import com.rebllelionandroid.features.sectorsList.databinding.ActivitySectorsListBinding
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SectorsListFragment: Fragment() {
+
+    private var currentGameStateId: Long = 0
+    private lateinit var gameStateWithSectors: LiveData<GameStateWithSectors>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +27,25 @@ class SectorsListFragment: Fragment() {
     ): View {
         val root = inflater.inflate(R.layout.fragment_sectors_list, container, false)
 
-        root.findViewById<MaterialButton>(R.id.btn_goto_detail).setOnClickListener {
-            root.findNavController().navigate(R.id.third_graph)
+        val gameStateViewModel = (activity as BaseActivity).gameStateViewModel
+        gameStateWithSectors = gameStateViewModel.gameState
+        gameStateWithSectors.observe(viewLifecycleOwner , {
+            root.findViewById<TextView>(R.id.sectorlist_text).text = it.gameState.id.toString()
+        })
+
+//        root.findViewById<MaterialButton>(R.id.btn_goto_detail).setOnClickListener {
+//            root.findNavController().navigate(R.id.third_graph)
+//        }
+        val gameStateSharedPrefFile = getString(R.string.gameStateSharedPrefFile)
+        val keyCurrentGameId = getString(R.string.keyCurrentGameId)
+        val sharedPref = activity?.getSharedPreferences(gameStateSharedPrefFile, Context.MODE_PRIVATE)
+        if(sharedPref?.contains(keyCurrentGameId) == true) {
+            currentGameStateId = sharedPref.getLong(keyCurrentGameId, 0)
+            viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                gameStateViewModel.loadAllGameStateWithSectors(currentGameStateId)
+            }
+        } else {
+            println("ERROR No current game ID found in shared preferences")
         }
 
         return root
