c5c5113f6861abb221a7600a709d6bdfb6ca69e8
Shawn Cook <sfncook@gmail.com>
2020-12-04 21:12:20 -0800

    cleanup

--- features/newgameactivity/src/main/java/com/rebellionandroid/features/newgameactivity/NewGameFragment.kt
+++ features/newgameactivity/src/main/java/com/rebellionandroid/features/newgameactivity/NewGameFragment.kt
@@ -42,13 +42,8 @@ class NewGameFragment: Fragment() {
 
     private fun updateList(view: View) {
         gameStateViewModel.getAllGameStates { allGameStates ->
-//            val sortedGameStates = it.toSortedSet(Comparator { s1, s2 ->
-//                s1.gameTime.compareTo(s2.gameTime)
-//            })
-//            val sortedGameStatesArr = ArrayList(sortedGameStates)
             val sortedGameStatesArr = allGameStates.sortedBy { gameState -> gameState.gameStartedTime }
             val viewAdapter = GameListAdapter(sortedGameStatesArr)
-//            val viewAdapter = GameListAdapter(it)
             val listGames = view.findViewById<RecyclerView>(R.id.list_games)
             viewLifecycleOwner.lifecycleScope.launch {
                 listGames.adapter = viewAdapter
