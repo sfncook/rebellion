59ff01012638d66a10819252c16b7cd44da0da24
Shawn Cook <sfncook@gmail.com>
2020-12-05 20:20:36 -0800

    fix layout

--- features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetUnitsFragment.kt
+++ features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetUnitsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.rebllelionandroid.core.BaseActivity
 import com.rebllelionandroid.core.GameStateViewModel
@@ -21,11 +23,15 @@ class PlanetUnitsFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_planet_units, container, false)
         selectedPlanetId = arguments?.getLong("planetId")!!
 
+        val textLoyaltyPercTeamA = root.findViewById<TextView>(R.id.units_text_loyalty_perc_team_a)
+        val textLoyaltyPercTeamB = root.findViewById<TextView>(R.id.units_text_loyalty_perc_team_b)
+        val planetLoyaltyImg = root.findViewById<ImageView>(R.id.units_planet_loyalty)
+
         gameStateViewModel = (activity as BaseActivity).gameStateViewModel
         val gameStateWithSectors = gameStateViewModel.gameState
         gameStateWithSectors.observe(viewLifecycleOwner , {
             val planetWithUnits = gameStateViewModel.getPlanetWithUnits(selectedPlanetId) {
-
+                val planet = it.planet
             }
 //            updatePlanetDetail(planetWithUnits)
         })
