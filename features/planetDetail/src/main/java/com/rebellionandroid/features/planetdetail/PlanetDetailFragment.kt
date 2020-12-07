bc709b79aea0b858c3d2e75851acf982d9463894
Shawn Cook <sfncook@gmail.com>
2020-12-06 19:09:14 -0800

    loadUnitsFrag

--- features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetDetailFragment.kt
+++ features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetDetailFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class PlanetDetailFragment: Fragment() {
     private var selectedPlanetId: Long = 0
@@ -18,16 +20,24 @@ class PlanetDetailFragment: Fragment() {
         val root = inflater.inflate(R.layout.fragment_planet_detail, container, false)
         selectedPlanetId = arguments?.getLong("planetId")!!
 
+        val planetUnitsFragment = PlanetUnitsFragment()
+        val planetFactoriesFragment = PlanetFactoriesFragment()
+
+        // Init Units view
+        loadUnitsFrag(planetUnitsFragment, "PlanetUnitsFragment")
+
         val bottomNavigation: BottomNavigationView = root.findViewById(R.id.planet_detail_bottom_nav)
 //        BottomNavigationView.OnNavigationItemSelectedListener { item ->
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.navigation_planet_units -> {
                     println("click units")
+                    loadUnitsFrag(planetUnitsFragment, "PlanetUnitsFragment")
                     true
                 }
                 R.id.navigation_planet_factories -> {
                     println("click factories")
+                    loadUnitsFrag(planetFactoriesFragment, "PlanetFactoriesFragment")
                     true
                 }
                 else -> false
@@ -35,5 +45,14 @@ class PlanetDetailFragment: Fragment() {
         }
 
         return root
+    }
+
+    private fun loadUnitsFrag(fragment: Fragment, tag: String) {
+        val bundle = bundleOf("planetId" to selectedPlanetId)
+        fragment.arguments = bundle
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.planet_detail_body, fragment, tag)
+            .addToBackStack(null)
+            .commit()
     }
 }
