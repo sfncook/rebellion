d414d23b87938482c1f595e43ebffcab215896ed
Shawn Cook <sfncook@gmail.com>
2020-12-09 20:34:44 -0800

    UnitCmdDialogFragment layout

--- components/commands/src/main/java/com/rebellionandroid/components/commands/UnitCmdDialogFragment.kt
+++ components/commands/src/main/java/com/rebellionandroid/components/commands/UnitCmdDialogFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.button.MaterialButton
 
 
 class UnitCmdDialogFragment: DialogFragment() {
@@ -15,6 +17,11 @@ class UnitCmdDialogFragment: DialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         val root = inflater.inflate(R.layout.fragment_unit_cmd, container, false)
+
+        root.findViewById<TextView>(R.id.unit_cmd_title).text = "Move Ship"
+        root.findViewById<MaterialButton>(R.id.unit_cmd_close_btn).setOnClickListener {
+            dismiss()
+        }
 
         return root
     }
