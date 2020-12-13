cbec698e4fc765598905ca24dc47876e620dc03b
Shawn Cook <sfncook@gmail.com>
2020-12-12 20:41:09 -0800

    working on command interface

--- components/commands/src/main/java/com/rebellionandroid/components/commands/UnitCmdDialogFragment.kt
+++ components/commands/src/main/java/com/rebellionandroid/components/commands/UnitCmdDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.RecyclerView
@@ -13,24 +14,24 @@ import com.rebllelionandroid.core.BaseActivity
 
 class UnitCmdDialogFragment: DialogFragment() {
 
-    private lateinit var listPlanets: RecyclerView
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val root = inflater.inflate(R.layout.fragment_unit_cmd, container, false)
+        val gameStateViewModel = (activity as BaseActivity).gameStateViewModel
 
-        root.findViewById<TextView>(R.id.unit_cmd_title).text = "Move Ship"
         root.findViewById<MaterialButton>(R.id.unit_cmd_close_btn).setOnClickListener {
             dismiss()
         }
-        listPlanets = root.findViewById(R.id.unit_cmd_list_1)
 
-        val gameStateViewModel = (activity as BaseActivity).gameStateViewModel
+        root.findViewById<Button>(R.id.unit_cmd_btn_sabotage).setOnClickListener{
+
+        }
+
         gameStateViewModel.getAllPlanets() { planets ->
-            listPlanets.adapter = PlanetListAdapter(planets)
+//            listPlanets.adapter = PlanetListAdapter(planets)
         }
 
         return root
