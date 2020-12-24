4009268825526c4ce6ec0dce4b93aab3e1288b09
Shawn Cook <sfncook@gmail.com>
2020-12-23 16:53:32 -0800

    limit back button

--- app/src/main/java/com/rebllelionandroid/MainActivity.kt
+++ app/src/main/java/com/rebllelionandroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rebllelionandroid
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,7 +12,6 @@ import com.rebllelionandroid.core.di.modules.ContextModule
 
 class MainActivity : BaseActivity() {
 
-    var fragDash: Boolean = true
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +21,15 @@ class MainActivity : BaseActivity() {
 
         setSupportActionBar(findViewById(R.id.main_toolbar))
         navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_dashboard,
-            R.id.sectors_list_graph,
-            R.id.sector_detail_graph,
-            R.id.planet_detail_graph,
-        ))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_dashboard,
+                R.id.sectors_list_graph,
+                R.id.sector_detail_graph,
+                R.id.planet_detail_graph,
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
-
     }
 
     private fun initAppDependencyInjection() {
@@ -37,5 +38,14 @@ class MainActivity : BaseActivity() {
             .contextModule(ContextModule(applicationContext))
             .build()
         gameStateViewModel = gameStateComponent.gameStateViewModel()
+    }
+
+    override fun onBackPressed() {
+        val curDest = navController.currentDestination
+        if(curDest?.id != R.id.fragment_sectors_list) {
+            super.onBackPressed()
+        } else {
+            // Do nothing - Sectors list has no back behavior
+        }
     }
 }
