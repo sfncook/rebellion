04eb3906b93cc12680b9af581f1808f022bb2921
Shawn Cook <sfncook@gmail.com>
2020-12-24 13:35:36 -0800

    populateShipsInSectorUi in sector list

--- components/entityUi/src/main/java/com/rebellionandroid/components/entityUi/SectorListAdapter.kt
+++ components/entityUi/src/main/java/com/rebellionandroid/components/entityUi/SectorListAdapter.kt
@@ -12,7 +12,7 @@ import com.rebllelionandroid.core.database.gamestate.SectorWithPlanets
 import com.rebllelionandroid.core.database.staticTypes.enums.TeamLoyalty
 
 class SectorListAdapter(
-    private val sectors: List<SectorWithPlanets>,
+    private val allSectorsWithPlanets: List<SectorWithPlanets>,
     private val myTeam: TeamLoyalty,
     private val onSelectCallback: (sectorId: Long) -> Unit
 ) : RecyclerView.Adapter<SectorListAdapter.ViewHolder>() {
@@ -31,7 +31,7 @@ class SectorListAdapter(
         val shipsInSectorImg: ImageView = view.findViewById(R.id.ships_in_sector_img)
 
         val manyEnemyShipsInSectorTxt: TextView = view.findViewById(R.id.many_enemy_ships_in_sector_txt)
-        val enemShipsInSectorImg: ImageView = view.findViewById(R.id.enemy_ships_in_sector_img)
+        val enemyShipsInSectorImg: ImageView = view.findViewById(R.id.enemy_ships_in_sector_img)
 
         init {
             view.setOnClickListener {
@@ -45,49 +45,29 @@ class SectorListAdapter(
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.list_item_sector, viewGroup, false)
-        return ViewHolder(view, sectors, onSelectCallback)
+        return ViewHolder(view, allSectorsWithPlanets, onSelectCallback)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val sector = sectors[position].sector
+        val sector = allSectorsWithPlanets[position].sector
         viewHolder.sectorName.text = sector.name
 
-        val planets = sectors[position].planets
+        val planets = allSectorsWithPlanets[position].planets
         val viewAdapter = SectorItemPlanetsListAdapter(Utilities.sortPlanets(planets))
         viewHolder.planetsList.adapter = viewAdapter
         viewHolder.planetsList.apply {
             adapter = viewAdapter
         }
 
-        var manyMyShips = 0
-        var manyEnemyShips = 0
-        planets.forEach { planetWithUnits ->
-            val myShips = planetWithUnits.shipsWithUnits.filter { shipWithUnits -> shipWithUnits.ship.team == myTeam }
-            val enemyShips = planetWithUnits.shipsWithUnits.filter { shipWithUnits -> shipWithUnits.ship.team != myTeam }
-            manyMyShips += myShips.size
-            manyEnemyShips += enemyShips.size
-        }
-        viewHolder.manyShipsInSectorTxt.text = manyMyShips.toString()
-        viewHolder.manyEnemyShipsInSectorTxt.text = manyEnemyShips.toString()
-
-        val myColor = when(myTeam) {
-            TeamLoyalty.TeamA -> R.color.loyalty_team_a
-            TeamLoyalty.TeamB -> R.color.loyalty_team_b
-            else -> R.color.loyalty_neutral
-        }
-        val enemyColor = when(myTeam) {
-            TeamLoyalty.TeamA -> R.color.loyalty_team_b
-            TeamLoyalty.TeamB -> R.color.loyalty_team_a
-            else -> R.color.loyalty_neutral
-        }
-        viewHolder.shipsInSectorImg.setColorFilter(
-            ContextCompat.getColor(viewHolder.shipsInSectorImg.context, myColor), android.graphics.PorterDuff.Mode.MULTIPLY
-        )
-        viewHolder.enemShipsInSectorImg.setColorFilter(
-            ContextCompat.getColor(viewHolder.shipsInSectorImg.context, enemyColor), android.graphics.PorterDuff.Mode.MULTIPLY
+        Utilities.populateShipsInSectorUi(
+            allSectorsWithPlanets[position],
+            viewHolder.manyShipsInSectorTxt,
+            viewHolder.shipsInSectorImg,
+            viewHolder.manyEnemyShipsInSectorTxt,
+            viewHolder.enemyShipsInSectorImg
         )
     }
 
-    override fun getItemCount() =  sectors.size
+    override fun getItemCount() =  allSectorsWithPlanets.size
 
 }
