e4e58677ccb7c69203118b82920ad6fba4f2c79f
Shawn Cook <sfncook@gmail.com>
2020-12-24 21:38:08 -0800

    fix FactoryBuildDialogFragment

--- components/commands/src/main/java/com/rebellionandroid/components/commands/FactoryBuildDialogFragment.kt
+++ components/commands/src/main/java/com/rebellionandroid/components/commands/FactoryBuildDialogFragment.kt
@@ -6,12 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ExpandableListView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.button.MaterialButton
 import com.rebllelionandroid.core.BaseActivity
 import com.rebllelionandroid.core.database.gamestate.Factory
 import com.rebllelionandroid.core.database.gamestate.GameStateWithSectors
+import com.rebllelionandroid.core.database.gamestate.enums.FactoryBuildTargetType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -23,6 +25,13 @@ class FactoryBuildDialogFragment: DialogFragment() {
     private var lastGroupExpandedPos: Int = -1
     private var selectedFactoryId: Long = 0
     private lateinit var selectedFactory: Factory
+    private var selectedBuildTargetType = FactoryBuildTargetType.ConstructionYard
+
+    private lateinit var buildBtnConstructionYard: MaterialButton
+    private lateinit var buildBtnShipYard: MaterialButton
+    private lateinit var buildBtnTrainingFacility: MaterialButton
+    private lateinit var buildBtnOrbitalBattery: MaterialButton
+    private lateinit var buildBtnPlanetaryShield: MaterialButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -66,12 +75,32 @@ class FactoryBuildDialogFragment: DialogFragment() {
             lastGroupExpandedPos = groupPosition;
         }
 
-        sectorsAndPlanetsExpandableList.setOnChildClickListener { _, _, _, _, planetId ->
-//            if(selectedShipWithUnits.ship.locationPlanetId != planetId) {
-//                gameStateViewModel.startShipJourneyToPlanet(selectedShipId, planetId, currentGameStateId)
-//                dismiss()
-//            }
-            true
+        buildBtnConstructionYard = root.findViewById(R.id.factmove_build_constructionyard)
+        buildBtnShipYard = root.findViewById(R.id.factmove_build_shipyard)
+        buildBtnTrainingFacility = root.findViewById(R.id.factmove_build_trainingfacility)
+        buildBtnOrbitalBattery = root.findViewById(R.id.factmove_build_orbitalbattery)
+        buildBtnPlanetaryShield = root.findViewById(R.id.factmove_build_planetaryshield)
+        updateBuildBtns(root.context)
+
+        buildBtnConstructionYard.setOnClickListener {
+            selectedBuildTargetType = FactoryBuildTargetType.ConstructionYard
+            updateBuildBtns(root.context)
+        }
+        buildBtnShipYard.setOnClickListener {
+            selectedBuildTargetType = FactoryBuildTargetType.ShipYard
+            updateBuildBtns(root.context)
+        }
+        buildBtnTrainingFacility.setOnClickListener {
+            selectedBuildTargetType = FactoryBuildTargetType.TrainingFacility
+            updateBuildBtns(root.context)
+        }
+        buildBtnOrbitalBattery.setOnClickListener {
+            selectedBuildTargetType = FactoryBuildTargetType.OrbitalBattery
+            updateBuildBtns(root.context)
+        }
+        buildBtnPlanetaryShield.setOnClickListener {
+            selectedBuildTargetType = FactoryBuildTargetType.PlanetaryShield
+            updateBuildBtns(root.context)
         }
 
         return root
@@ -94,5 +123,40 @@ class FactoryBuildDialogFragment: DialogFragment() {
         val selectedSector = sortedSectors.find { sectorWithPlanets -> sectorWithPlanets.sector.id == selectedSectorId }
         val indexOfSelectedSector = sortedSectors.indexOf(selectedSector)
         sectorsAndPlanetsExpandableList.expandGroup(indexOfSelectedSector)
+    }
+
+    private fun updateBuildBtns(context: Context) {
+        val unselectedColor = ContextCompat.getColor(context, R.color.list_item_bg)
+        val unselectedTextColor = ContextCompat.getColor(context, R.color.purple_700)
+        buildBtnConstructionYard.setBackgroundColor(unselectedColor)
+        buildBtnShipYard.setBackgroundColor(unselectedColor)
+        buildBtnTrainingFacility.setBackgroundColor(unselectedColor)
+        buildBtnOrbitalBattery.setBackgroundColor(unselectedColor)
+        buildBtnPlanetaryShield.setBackgroundColor(unselectedColor)
+
+        buildBtnConstructionYard.setTextColor(unselectedTextColor)
+        buildBtnShipYard.setTextColor(unselectedTextColor)
+        buildBtnTrainingFacility.setTextColor(unselectedTextColor)
+        buildBtnOrbitalBattery.setTextColor(unselectedTextColor)
+        buildBtnPlanetaryShield.setTextColor(unselectedTextColor)
+
+        val selectedColor = ContextCompat.getColor(context, R.color.purple_200)
+        val selectedTextColor = ContextCompat.getColor(context, R.color.white)
+        when(selectedBuildTargetType) {
+            FactoryBuildTargetType.ConstructionYard -> buildBtnConstructionYard.setBackgroundColor(selectedColor)
+            FactoryBuildTargetType.ShipYard -> buildBtnShipYard.setBackgroundColor(selectedColor)
+            FactoryBuildTargetType.TrainingFacility -> buildBtnTrainingFacility.setBackgroundColor(selectedColor)
+            FactoryBuildTargetType.OrbitalBattery -> buildBtnOrbitalBattery.setBackgroundColor(selectedColor)
+            FactoryBuildTargetType.PlanetaryShield -> buildBtnPlanetaryShield.setBackgroundColor(selectedColor)
+            else -> {}
+        }
+        when(selectedBuildTargetType) {
+            FactoryBuildTargetType.ConstructionYard -> buildBtnConstructionYard.setTextColor(selectedTextColor)
+            FactoryBuildTargetType.ShipYard -> buildBtnShipYard.setTextColor(selectedTextColor)
+            FactoryBuildTargetType.TrainingFacility -> buildBtnTrainingFacility.setTextColor(selectedTextColor)
+            FactoryBuildTargetType.OrbitalBattery -> buildBtnOrbitalBattery.setTextColor(selectedTextColor)
+            FactoryBuildTargetType.PlanetaryShield -> buildBtnPlanetaryShield.setTextColor(selectedTextColor)
+            else -> {}
+        }
     }
 }
