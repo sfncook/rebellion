998da3426a371f5d7250fd26234b2fb6dc341ca5
Shawn Cook <sfncook@gmail.com>
2020-12-24 18:16:56 -0800

    StructureListAdapter

--- features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetFactoriesFragment.kt
+++ features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/PlanetFactoriesFragment.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.launch
 class PlanetFactoriesFragment : Fragment() {
     private var selectedPlanetId: Long = 0
     private lateinit var listFactories: RecyclerView
-    private lateinit var listEmptyEnergies: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -24,35 +23,19 @@ class PlanetFactoriesFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_planet_factories, container, false)
         selectedPlanetId = arguments?.getLong("planetId")!!
         listFactories = root.findViewById(R.id.list_factories)
-        listEmptyEnergies = root.findViewById(R.id.list_empty_energy)
 
         val gameStateViewModel = (activity as BaseActivity).gameStateViewModel
         val gameStateWithSectors = gameStateViewModel.gameState
         gameStateWithSectors.observe(viewLifecycleOwner , {
             gameStateViewModel.getPlanetWithUnits(selectedPlanetId) { planetWithUnits ->
-                updateFactoriesList(planetWithUnits.factories)
-                val manyEmptyEnergies = planetWithUnits.planet.energyCap - planetWithUnits.factories.size
-                println("manyEmptyEnergies: $manyEmptyEnergies")
-                updateEmptyEnergyList(manyEmptyEnergies)
+                val viewAdapter = StructureListAdapter(planetWithUnits)
+                viewLifecycleOwner.lifecycleScope.launch {
+                    listFactories.adapter = viewAdapter
+                }
             }
 
         })
-
         return root
     }
 
-    private fun updateFactoriesList(factories: List<Factory>) {
-        val viewAdapter = FactoryListAdapter(factories)
-        viewLifecycleOwner.lifecycleScope.launch {
-            listFactories.adapter = viewAdapter
-        }
-    }
-
-    private fun updateEmptyEnergyList(manyEmptyEnergies: Int) {
-        val emptyEnergies = (IntArray(manyEmptyEnergies) { it }).toCollection(ArrayList())
-        val viewAdapter = EmptyEnergyListAdapter(emptyEnergies)
-        viewLifecycleOwner.lifecycleScope.launch {
-            listEmptyEnergies.adapter = viewAdapter
-        }
-    }
 }
---
+++ features/planetDetail/src/main/java/com/rebellionandroid/features/planetdetail/StructureListAdapter.kt
@@ -0,0 +1,108 @@
+package com.rebellionandroid.features.planetdetail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rebllelionandroid.core.database.gamestate.DefenseStructure
+import com.rebllelionandroid.core.database.gamestate.Factory
+import com.rebllelionandroid.core.database.gamestate.PlanetWithUnits
+import com.rebllelionandroid.core.database.gamestate.enums.DefenseStructureType
+import com.rebllelionandroid.core.database.gamestate.enums.FactoryType
+
+class StructureListAdapter(
+    planetWithUnits: PlanetWithUnits
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val structuresList = mutableListOf<Any>()
+
+    enum class StructureType(val value: Int) {
+        Factory(1),
+        Defense(2),
+        Empty(3),
+    }
+
+    private val EMPTY_ENERGY = "EMPTY_ENERGY"
+
+    init {
+        structuresList.addAll(planetWithUnits.factories)
+        structuresList.addAll(planetWithUnits.defenseStructures)
+        val manyEnergyUsed = (planetWithUnits.factories.size + planetWithUnits.defenseStructures.size)
+        val manyEnergyEmpty = planetWithUnits.planet.energyCap - manyEnergyUsed
+        for (i in 1 .. manyEnergyEmpty) {
+            structuresList.add(EMPTY_ENERGY)
+        }
+    }
+
+    class FactoryViewHolder(view: View, structureList: List<Any>) : RecyclerView.ViewHolder(view) {
+        val factoryLabel: TextView = view.findViewById(R.id.factory_label)
+        val factoryImg: ImageView = view.findViewById(R.id.factory_img)
+    }
+
+    class DefenseViewHolder(view: View, structureList: List<Any>) : RecyclerView.ViewHolder(view) {
+        val label: TextView = view.findViewById(R.id.unit_label)
+        val img: ImageView = view.findViewById(R.id.unit_img)
+    }
+
+    class EmptyViewHolder(view: View, structureList: List<Any>) : RecyclerView.ViewHolder(view)
+
+    override fun getItemViewType(position: Int): Int {
+        val structure = structuresList[position]
+        return when(structure::class) {
+            Factory::class -> StructureType.Factory.value
+            DefenseStructure::class -> StructureType.Defense.value
+            else -> StructureType.Empty.value
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutId = when(viewType) {
+            StructureType.Factory.value -> R.layout.list_item_factory
+            StructureType.Defense.value -> R.layout.list_item_img_with_title
+            else -> R.layout.list_item_empty_energy
+        }
+
+        val view = LayoutInflater.from(viewGroup.context).inflate(layoutId, viewGroup, false)
+
+        return when(viewType) {
+            StructureType.Factory.value -> FactoryViewHolder(view, structuresList)
+            StructureType.Defense.value -> DefenseViewHolder(view, structuresList)
+            else -> EmptyViewHolder(view, structuresList)
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        val structure = structuresList[position]
+
+        when(structure::class) {
+            Factory::class -> {
+                val factory = structure as Factory
+                val factoryViewHolder = viewHolder as FactoryViewHolder
+                factoryViewHolder.factoryLabel.text = factory.factoryType.value
+                val imgSrc  = when(factory.factoryType) {
+                    FactoryType.ConstructionYard -> R.drawable.factory_ctor_yard
+                    FactoryType.ShipYard -> R.drawable.factory_ship_yard
+                    FactoryType.TrainingFaciliy -> R.drawable.factory_training_facility
+                }
+                factoryViewHolder.factoryImg.setImageResource(imgSrc)
+            }
+            DefenseStructure::class -> {
+                val defenseStructure = structure as DefenseStructure
+                val defenseViewHolder = viewHolder as DefenseViewHolder
+                defenseViewHolder.label.text = defenseStructure.defenseStructureType.value
+                val imgSrc  = when(defenseStructure.defenseStructureType) {
+                    DefenseStructureType.PlanetaryShield -> R.drawable.planetary_shield
+                    DefenseStructureType.OrbitalBattery -> R.drawable.orbital_battery
+                }
+                defenseViewHolder.img.setImageResource(imgSrc)
+            }
+            EMPTY_ENERGY::class -> {}
+            else -> {}
+        }
+    }
+
+    override fun getItemCount() =  structuresList.size
+
+}
